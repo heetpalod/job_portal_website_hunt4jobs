b9ec1c1bc9bd112754aea667c82d6db3bf701639
varunkandukuri24 <39822010+varunkandukuri24@users.noreply.github.com>
2020-06-03 23:48:33 -0700

    Add files via upload

--- add_skill.php
+++ add_skill.php
@@ -1,59 +1,126 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<title><h2>Add Personal Details</h2></title>
+  <title>Add Skill Details</title>
 </head>
 
-<body>
-<?php
-//including the database connection file
-include_once("config.php");
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-if(isset($_POST['submit_education'])) {	
-	$degree_name = mysqli_real_escape_string($mysqli, $_POST['degree_name']);
-	$major_name = mysqli_real_escape_string($mysqli, $_POST['major_name']);
-	$university_name = mysqli_real_escape_string($mysqli, $_POST['university_name']);
-	$grad_date = mysqli_real_escape_string($mysqli, $_POST['grad_date']);
-	$cgpa = mysqli_real_escape_string($mysqli, $_POST['cgpa']);
-		
-	// checking empty fields
-	if(empty($degree_name) || empty($major_name) || empty($university_name) || empty($grad_date) || empty($cgpa)) {
-				
-		if(empty($degree_name)) {
-			echo "<font color='red'>Degree Name field is empty.</font><br/>";
-		}
-		
-		if(empty($major_name)) {
-			echo "<font color='red'>Major Name field is empty.</font><br/>";
-		}
-		
-		if(empty($university_name)) {
-			echo "<font color='red'>University Name field is empty.</font><br/>";
-		}
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-		if(empty($grad_date)) {
-			echo "<font color='red'>Graduation Date field is empty.</font><br/>";
-		}
-
-		if(empty($cgpa)) {
-			echo "<font color='red'>CGPA field is empty.</font><br/>";
-		}
-			  
-		//link to the previous page
-		echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
-	} 
-
-	else { 
-		// if all the fields are filled (not empty) 
-		
-		//insert data to database	
-		$result = mysqli_query($mysqli, "INSERT INTO candidate_education(Degree, Major, University, Graduation_Date, CGPA) VALUES('$degree_name', '$major_name','$university_name','$grad_date','$cgpa')");
-		
-		//display success message
-
-		echo "<font color='green'>Data added successfully.";
-		//echo "<br/><a href='index.php'>View Result</a>";
-	}
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<style>
+  * {
+  margin: 0px;
+  padding: 0px;
 }
+body {
+  font-size: 120%;
+  background: #F8F8FF;
+}
+
+.header {
+  width: 30%;
+  margin: 50px auto 0px;
+  color: white;
+  background: #5F9EA0;
+  text-align: center;
+  border: 1px solid #B0C4DE;
+  border-bottom: none;
+  border-radius: 10px 10px 0px 0px;
+  padding: 20px;
+}
+form, .content {
+  width: 30%;
+  margin: 0px auto;
+  padding: 20px;
+  border: 1px solid #B0C4DE;
+  background: white;
+  border-radius: 0px 0px 10px 10px;
+}
+.input-group {
+  margin: 10px 0px 10px 0px;
+}
+.input-group label {
+  display: block;
+  text-align: left;
+  margin: 3px;
+}
+.input-group input {
+  height: 30px;
+  width: 93%;
+  padding: 5px 10px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid gray;
+}
+.btn {
+  padding: 10px;
+  font-size: 15px;
+  color: white;
+  background: #5F9EA0;
+  border: none;
+  border-radius: 5px;
+}
+.error {
+  width: 92%;
+  margin: 0px auto;
+  padding: 10px;
+  border: 1px solid #a94442;
+  color: #a94442;
+  background: #f2dede;
+  border-radius: 5px;
+  text-align: left;
+}
+.success {
+  color: #3c763d;
+  background: #dff0d8;
+  border: 1px solid #3c763d;
+  margin-bottom: 20px;
+}
+  </style>
+<body>
+  <a href="index.php" style ="padding-left: 20px; float:left;"><h3>Home</h3></a>
+  <form action="add_skill.php" method="post" text-align="center" name="form4">
+     <div class="form-group">
+        <div class ="input-group">
+              <h4> Add Skills </h4>
+    <?php
+    include_once("config.php");
+
+$result = mysqli_query($mysqli, "SELECT * FROM skillset");
+foreach($result as $row)
+{
+  echo "<input type='checkbox' name='skillset[]' value='".$row['Skillset_ID']."'>";
+  echo $row['Skill'];
+  echo "<br>";
+}
+
+
+
 ?>
+</div>
+    </div>
+
+      <input type="submit" class="btn btn-info" name="submit_skill" value="Add">
+
+  </form>
 </body>
 </html>
+
+<?php
+if(isset($_POST['submit_skill']))
+{
+  $all_skills = $_POST['skillset'];
+  foreach ($all_skills as $skill)
+  {
+    $result = mysqli_query($mysqli, "INSERT INTO candidate_skillset(User_ID, Skillset_ID) VALUES('$skill')");
+}
+echo "<font color='green'>Data added successfully.";
+}
+?>
+
+
